aac06edc35e5ae886657017e6cb3070abc87619f
Jesus Martinez <mtzgtzjesus@gmail.com>
2023-07-26 21:57:50 -0500

    feat: random walk

--- ch6/main.py
+++ ch6/main.py
@@ -11,15 +11,14 @@ draw_g(G)
 G.nodes
 
 
-previous = 0
-current = 8
-p=1
-q=1
+# previous = 0
+# current = 8
+# p=1
+# q=1
 
 # How to get the 
 def next_node(G, previous, current, p, q):
     neighbors = list(G.neighbors(current))
-    
     
     # pi_{i, j} is the transition prob of moving to next neighbor. 
     # this is decompsed as pi = alpha  * omega
@@ -40,73 +39,34 @@ def next_node(G, previous, current, p, q):
     
     probs = [alpha / sum(alphas) for alpha in alphas]
 
+    next = np.random.choice(neighbors, size=1, p=probs)[0]
+    return next
 
-import numpy as np
-from scipy.optimize import curve_fit
-import matplotlib.pyplot as plt
+def random_walk(start, length, p, q):
+    walk = [start]
+    for i in range(length):
+        current = walk[-1]
+        previous = walk[-2] if len(walk) > 1 else None
+        next = next_node(previous, current, p, q)
+        walk.append(next)
+    return [str(x) for x in walk]
 
-# Define the model
-def exponential_decline(t, q0, b):
-  return q0 * np.exp(-b * t)
+# next_ = next_node(G, 4, 3, 1, 1)
+random_walk(0, 8, p=1, q=1)
+draw_g(G)
 
-# Create the data
-t = np.linspace(0, 100, 1000)
-q = exponential_decline(t, 100, 0.05)
 
-# Add noise to the data
-noise = np.random.normal(0, 5, len(q))
-q += noise
 
-# Introduce gaps in the data
-gap_probability = 0.1
-for i in range(len(q)):
-  if random.random() < gap_probability:
-    q[i] = np.nan
 
-# Simulate a production decline
-for i in range(len(q)):
-  if random.random() < 0.5:
-    q[i] -= random.uniform(0, 10)
 
-# Plot the data
-plt.plot(t, q)
-plt.show()
 
-# Fit the model to the data
-popt, _ = curve_fit(exponential_decline, t, q)
 
-# Get the optimal value of -b
-b = -popt[1]
 
-# Print the optimal value of -b
-print('The optimal value of -b is:', b)
 
-import random
 
-# Create the data
-t = np.linspace(0, 100, 1000)
-q = exponential_decline(t, 100, 0.05)
 
-# Add noise to the data
-noise = np.random.normal(0, 5 / len(q), len(q))
-q += noise
 
-# Calculate the drop size
-drop_size = random.uniform(0, 100)
 
-# Define the event probability
-event_probability = 0.1
 
-# Initialize the duration variable
-duration = int(random.uniform(10, 100))
 
-# Add the random event
-for i in range(len(q) - duration):
-  if random.random() < event_probability:
-    q[i] -= drop_size
-    for j in range(i, i+duration):
-      q[j] = 0
 
-# Plot the data
-plt.plot(t, q)
-plt.show()

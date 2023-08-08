b37d3c523fb4bcef0d60c470e762489ac1dc35b8
Jesus Martinez <mtzgtzjesus@gmail.com>
2023-08-08 05:41:50 -0500

    feat:

---
+++ ch6/intro.py
@@ -0,0 +1,22 @@
+import torch
+import numpy as np
+from torch_geometric.datasets import Planetoid
+from torch_geometric.utils import degree
+from collections import Counter
+import matplotlib.pyplot as plt
+
+dataset = Planetoid(root=".", name="cora")
+
+
+
+
+
+
+
+
+
+
+
+
+
+

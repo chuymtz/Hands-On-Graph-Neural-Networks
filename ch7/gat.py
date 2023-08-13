c497e99b194af19f424ae040fbfbfc6cf041a813
Jesus Martinez <mtzgtzjesus@gmail.com>
2023-08-13 13:55:33 -0500

    init: ch 7

---
+++ ch7/gat.py
@@ -0,0 +1,38 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+h_i = sum_{j in N_i} alpha_ij W x_j
+"""
+
+np.random.seed(0)
+A = np.array([
+    [1, 1, 1, 1],
+    [1, 1, 0, 0],
+    [1, 0, 1, 1],
+    [1, 0, 1, 1]
+])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
